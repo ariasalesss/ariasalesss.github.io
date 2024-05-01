a0c99ecab2de05152d45288deb831aab96210acb
ariasalesss <aria.saless@colorado.edu>
2024-04-30 20:38:17 -0600

    edited canvas height and text formatting

--- js-midterm/js-midterm.js
+++ js-midterm/js-midterm.js
@@ -6,6 +6,7 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+
 // generate random number between min and max 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
